a95054eab331d481d28ddc0e6c6d3ccb55387ba1
Avinash Tiwari <vnashtiwari7@gmail.com>
2020-02-20 00:16:24 +0530

    adding files

--- Evolent.Api/Controllers/ContactController.cs
+++ Evolent.Api/Controllers/ContactController.cs
@@ -5,6 +5,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Evolent.Services;
 using Evolent.Core.Models;
+using System.Web.Http;
+
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace Evolent.Api.Controllers
@@ -23,37 +25,53 @@ namespace Evolent.Api.Controllers
 
         // GET: api/<controller>
         [HttpGet]
-        public async Task<IEnumerable<Contact>> Get()
+        public async Task<ActionResult<IEnumerable<Contact>>> Get()
         {
-            return await _contactService.GetAllContact();
+               return Ok(await _contactService.GetAllContact());
         }
 
         // GET api/<controller>/5
         [HttpGet("{id}")]
-        public async Task<Contact> Get(int id)
+        public async Task<ActionResult<Contact>> Get(int id)
         {
-            return await _contactService.GetContactByID(id);
+            var contact = await _contactService.GetContactByID(id);
+            if (contact == null)
+            {
+                return NotFound();
+            }
+            return Ok(contact);
         }
 
         // POST api/<controller>
         [HttpPost]
-        public async Task<int> Post([FromBody]Contact contact)
+        public async Task<ActionResult<int>> Post([FromBody]Contact contact)
         {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
             return await _contactService.AddContact(contact);
 
         }
 
         [HttpPut]
-        public async Task<int> Put([FromBody]Contact contact)
+        public async Task<ActionResult<int>> Put([FromBody]Contact contact)
         {
-            return await _contactService.UpdateContact(contact);
+
+            return Ok(await _contactService.UpdateContact(contact));
         }
 
         // DELETE api/<controller>/5
         [HttpDelete("{id}")]
-        public async Task<int> Delete(int id)
+        public async Task<ActionResult<int>> Delete(int id)
         {
-            return await _contactService.DeleteContact(id);
+            var contact = _contactService.GetContactByID(id);
+
+            if (contact == null)
+            {
+                return NotFound();
+            }
+            return Ok(await _contactService.DeleteContact(id));
         }
     }
 }
